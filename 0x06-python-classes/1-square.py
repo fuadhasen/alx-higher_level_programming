4f72d66d507160acc243e1722e3cc27a961e00ca
fuadhasen <fuadhas6634@gmail.com>
2024-02-07 09:54:46 -0800

    squre size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py
+# fuadhasen
+"""define square class with size"""
+
+
+class Square:
+    """class with size private attribute"""
+    def __init__(self, size):
+        self.__size = size
