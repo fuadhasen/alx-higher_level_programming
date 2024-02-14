9bd0763cd61cc798c1b3c36bd4b6e37f5c797e87
fuadhasen <fuadhas6634@gmail.com>
2024-02-13 22:53:09 -0800

    del

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+# fuad hassen
+# 5-rectangle.py
+""" This module define class Rectangle """
+
+
+class Rectangle:
+    """ Rectangle class. """
+
+    def __init__(self, width=0, height=0):
+        """ intialize constructor.
+
+        Args:
+            width (int): frist argument
+            height (int): second argument
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieve width. """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width. """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve height. """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        res = self.__width + self.__height
+        return 2 * res
+
+    def __str__(self):
+        """return printable string (#) to the user. """
+        if self.__height == 0 or self.__width == 0:
+            return ""
+
+        res = ""
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                res += "#"
+            if i != self.__height - 1:
+                res += '\n'
+        return res
+
+    def __repr__(self):
+        """return official string representation object. """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """delete the instance and say Bye rectangle... """
+        del self.__height
+        del self.__width
+        print("Bye rectangle...")
