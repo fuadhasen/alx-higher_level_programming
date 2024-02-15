21bc162bce4f2feadd5a6165fb22d34ef8d25429
fuadhasen <fuadhas6634@gmail.com>
2024-02-15 00:30:18 -0800

    text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Fuad Hassen <fuadhas6634@gmail.com>
+# 5-text_indentation.py
+"""This module provide a function that indent text. """
+
+
+def text_indentation(text):
+    """This function indent the text.
+
+    Args:
+        text (string): the text to be indented
+
+    Raise:
+        TypeError if the text is not string
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i], end="")
+            print("\n")
+        else:
+            if text[i - 1] == '.' or text[i - 1] == '?' or text[i - 1] == ':':
+                continue
+            print(text[i], end="")
