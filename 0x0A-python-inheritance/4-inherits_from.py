4eacac4ceb0e691e54df941f8c3b5e59c94f5c6a
fuadhasen <fuadhas6634@gmail.com>
2024-02-19 14:50:12 -0800

    inherit update

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module povide a function that check obj 
+   is instance of class inherited from  parent class."""
+
+
+def inherits_from(obj, a_class):
+    """ function check wether given obj is instance class that
+        inherited from parent class but not from parent.
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
