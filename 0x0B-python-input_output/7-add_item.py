23aeafc51743a38c3eff510aa3ed0d49937e0e65
fuadhasen <fuadhas6634@gmail.com>
2024-02-23 04:26:54 -0800

    add item update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""This module provide function that Create object from a JSON file. """
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+ls = load_from_json_file(filename)
+if ls == None:
+    ls = []
+for i in range(1, len(sys.argv)):
+    ls.append(sys.argv[i])
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file(ls, filename)
