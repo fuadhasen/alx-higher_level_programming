3c32c6b90cbe20a0406de9ac406e862a142eeee2
fuadhasen <fuadhas6634@gmail.com>
2024-02-23 10:53:08 -0800

    add item fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """This module provide function that Create object from a JSON file. """
 import sys
+import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-ls = load_from_json_file(filename)
-if ls is None:
+if os.path.isfile(filename):
+    ls = load_from_json_file(filename)
+else:
     ls = []
+
 for i in range(1, len(sys.argv)):
     ls.append(sys.argv[i])
 
