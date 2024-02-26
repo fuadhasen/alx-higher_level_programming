ef9afbbbd51929acd2568e99c3eac9e4bbb76c50
fuadhasen <fuadhas6634@gmail.com>
2024-02-26 13:08:53 -0800

    update rectangle test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,14 +1,32 @@
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class test_rectangle(unittest.TestCase):
 
-    def test_rectangle(self):
-        r1 = Rectangle(10, 2, id=3)
-        self.assertEqual(r1.id, 3)
+    def setUp(self):
+        Base._Base__nb_object = 0
 
-    def test_exception_rec(self):
+    def tearDown(self):
+        pass
+
+    def test_rectangle(self):
+        r0 = Rectangle(10, 2)
+        self.assertEqual(r0.id, 1)
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.id, 2)
+        r3 = Rectangle(3, 2, id=None)
+        self.assertEqual(r3.id, 3)
+        r4 = Rectangle(3, 2, id=-4)
+        self.assertEqual(r4.id, -4)
+        self.assertEqual(r1.id, 2)
+        r = Rectangle(10, 2, id=8)
+        self.assertEqual(r.id, 8)
+
+    def test_exception(self):
         with self.assertRaises(TypeError):
-             r = Rectangle(10)
-        
+            r = Rectangle(10)
+
+        with self.assertRaises(TypeError):
+            r = Rectangle()        
