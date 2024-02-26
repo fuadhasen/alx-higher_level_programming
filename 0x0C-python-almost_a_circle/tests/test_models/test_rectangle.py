9e09b7530242f5d686089eb9210f0132628e7090
fuadhasen <fuadhas6634@gmail.com>
2024-02-26 11:54:43 -0800

    test rectangle update

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,3 +7,5 @@ class test_rectangle(unittest.TestCase):
     def test_without_id(self):
         r1 = Rectangle(10, 2, id=3)
         self.assertEqual(r1.id, 3)
+        r2 = Rectangle(10, 2)
+        self.assertEqual(r2.id, 2)
