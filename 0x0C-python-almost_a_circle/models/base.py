0cf7bfcaddbca8910eb537f8616eb8ca2e7ae01e
fuadhasen <fuadhas6634@gmail.com>
2024-02-27 03:01:35 -0800

    base update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,9 +18,17 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ to json representation. """
-        try:
-            if list_dictionaries is None or len(list_dictionaries) == 0:
-                return "[]"
-            return json.dumps(list_dictionaries)
-        except TypeError:
-            return "not serializable"
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        # befor iencoding convert
+        # none serializable to serializablw object
+        serialize_list = []
+        for i in list_dictionaries:
+            serialize_dict = {}
+            for k, v in i.items():
+                if not isinstance(v, (int, float, str, None, bool, list, dict)):
+                    serialize_dict[k] = str(v)
+                else:
+                    serialize_dict[k] = v
+            serialize_list.append(serialize_dict)
+        return json.dumps(serialize_list)
