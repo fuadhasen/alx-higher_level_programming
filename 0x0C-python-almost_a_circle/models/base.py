53daef0442de784fd7eb463a10ad2b46e5e7b5b9
fuadhasen <fuadhas6634@gmail.com>
2024-02-27 01:00:09 -0800

    base update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """ to json representation. """
         try:
-            if list_dictionaries is None or list_dictionaries == []:
+            if list_dictionaries is None or len(list_dictionaries) == 0:
                 return "[]"
             return json.dumps(list_dictionaries)
         except TypeError:
