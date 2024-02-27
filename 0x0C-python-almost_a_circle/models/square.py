feb9d33abbb7e79aff0af3cb46fd92534c3cd852
fuadhasen <fuadhas6634@gmail.com>
2024-02-26 22:26:06 -0800

    square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-"""This module provide class Square. """
+""" This module provide class Square. """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """square subclass of Rectangle. """
+    """ square subclass of Rectangle. """
     def __init__(self, size, x=0, y=0, id=None):
-        """subclass constructor. """
+        """ subclass constructor. """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ getter of size. """
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """ setter. """
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
 
     def __str__(self):
         """overriding the str. """
