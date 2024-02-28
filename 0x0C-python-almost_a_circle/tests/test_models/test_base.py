f170197f25a555af91d1469384ac6d65028654d4
fuadhasen <fuadhas6634@gmail.com>
2024-02-27 23:17:22 -0800

    test base fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,4 +4,20 @@ from models.base import Base
 
 class test_base(unittest.TestCase):
 
-     pass
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def tearDown(self):
+        pass
+
+    def test_base_creation(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(4)
+        self.assertEqual(b3.id, 4)
+        b4 = Base(None)
+        self.assertEqual(b4.id, 3)
+        b5 = Base(-4)
+        self.assertEqual(b5.id, -4)
