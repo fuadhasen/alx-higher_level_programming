3e42c2baacc895ea509eb4438a2ae10fa587cc78
fuadhasen <fuadhas6634@gmail.com>
2024-02-27 22:59:44 -0800

    test created

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,22 +4,4 @@ from models.base import Base
 
 class test_base(unittest.TestCase):
 
-    def test_equal(self):
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-
-    def test_with_id(self):
-        b = Base(12)
-        self.assertEqual(b.id, 12)
-
-    def test_nagative_input(self):
-        b = Base(-12)
-        self.assertEqual(b.id, -12)
-
-    def test_maxid(self):
-        with self.assertRaises(ValueError):
-            b = Base(float('ínf'))
-
-    def test_minid(self):
-        with self.assertRaises(ValueError):
-            b = Base(float('null')) 
+     pass
