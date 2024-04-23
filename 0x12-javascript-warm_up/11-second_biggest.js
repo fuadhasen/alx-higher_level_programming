b20d89f9f0111a9eee9e04d6ba76aac75d9cde00
fuadhasen <fuadhas6634@gmail.com>
2024-04-23 01:26:07 -0700

    second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 
-const arr = process.argv.slice(2);
-if (arr.length === 0) {
-  console.log('0');
-} else if (arr.length === 1) {
+const arr = process.argv.slice(2).map(Number);
+if (arr.length <= 1) {
   console.log('0');
 } else {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
+    for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        const temp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
