cd03c4d7e4df91275956f538cea41fbe2dca7070
fuadhasen <fuadhas6634@gmail.com>
2024-04-24 14:40:54 -0700

    square print

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,13 +2,13 @@
 
 class Square extends require('./5-square') {
   charPrint (c) {
-    if (c === undefined) {
+    if (c === undefined ) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         let r = '';
         for (let j = 0; j < this.width; j++) {
-          r += 'C';
+          r += c;
         }
         console.log(r);
       }
