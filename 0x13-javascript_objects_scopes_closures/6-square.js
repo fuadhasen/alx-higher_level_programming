70cc3db006c1989633f58aaab378e05523d76a9d
fuadhasen <fuadhas6634@gmail.com>
2024-04-24 13:57:28 -0700

    square print

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let r = '';
+        for (let j = 0; j < this.width; j++) {
+          r += 'C';
+        }
+        console.log(r);
+      }
+    }
+  }
+}
+module.exports = Square;
