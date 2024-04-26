f1ae654ea19598eba9bddbc0c579eca6501f1182
fuadhasen <fuadhas6634@gmail.com>
2024-04-26 08:13:17 -0700

    cities created

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module list all cities from DB. """
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cur = db.cursor()
+    query = """
+        SELECT *
+        FROM cities
+        order by cities.id
+    """
+
+    cur.execute(query)
+    res = cur.fetchall()
+    for row in res:
+        print(row)
