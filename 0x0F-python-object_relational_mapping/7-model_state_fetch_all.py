86c2948cf09fcd4358338e437952ec9d35ecee52
fuadhasen <fuadhas6634@gmail.com>
2024-04-29 02:08:26 +0530

    [UPDATE] Task 7: fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-""" Lists all State objects from the database """
+"""This module list all state objcet using ORM """
 
-if __name__ == "__main__":
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
+if __name__ == '__main__':
+    user_name = sys.argv[1]
+    user_passwd = sys.argv[2]
+    user_DB = sys.argv[3]
 
-    url = 'mysql+mysqldb://\
-{}:{}@localhost:3306/{}'.format(username, password, db_name)
-    engine = create_engine(url)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(user_name, user_passwd, user_DB),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
+    filter_users = session.query(State).order_by(State.id).all()
 
-    for state in session.query(State).order_by(State.id).all():
-        print(f"{state.id}: {state.name}")
+    for state in filter_users:
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
