4ad71df8bd5294705bc8312014949ff6183863bd
fuadhasen <fuadhas6634@gmail.com>
2024-04-28 14:59:35 +0530

    [UPDATE] Task 6: First model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 class State(Base):
     """state class defined."""
-    __tablename__ = 'state'
+    __tablename__ = 'states'
     id = Column(Integer,
                 primary_key=True,
                 autoincrement=True,
