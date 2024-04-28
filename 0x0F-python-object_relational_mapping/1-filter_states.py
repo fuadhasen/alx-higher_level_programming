3f61e708395b7c99b36fac92f627e6361db965cb
fuadhasen <fuadhas6634@gmail.com>
2024-04-28 12:23:49 +0530

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -25,3 +25,5 @@ if __name__ == '__main__':
     res = cur.fetchall()
     for row in res:
         print(row)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -27,3 +27,5 @@ if __name__ == '__main__':
     res = cur.fetchall()
     for row in res:
         print(row)
+    cur.close()
+    db.close()
