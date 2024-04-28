cd9973ee11625e4f27293f6f27f23d03923e65ae
fuadhasen <fuadhas6634@gmail.com>
2024-04-29 00:47:43 +0530

    [ADD] Task 7: model_state fetch all

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module list all state objcet using ORM """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    user_name = sys.argv[1]
+    user_passwd = sys.argv[2]
+    user_DB = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        user_name, user_passwd, user_DB
+    ))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    filter_users = session.query(State).order_by(State.id).all()
+
+    for state in filter_users:
+        print("{}: {}".format(state.id, state.name))
