beebc839cac61252704a474197c3e2f1acd772f4
fuadhasen <fuadhas6634@gmail.com>
2024-04-28 14:54:18 +0530

    [UPDATE] Task 6: First model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 """This module define First state model"""
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
