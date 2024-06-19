b810eb41719731b4fcb003c68c379d227fb5efbd
fuadhasen <fuadhas6634@gmail.com>
2024-06-19 10:00:10 -0700

    square updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -13,7 +13,7 @@ class Square extends Rectangle {
       for (let i = 0; i < this.height; i++) {
         let str = '';
         for (let j = 0; j < this.width; j++) {
-          str += 'c';
+          str += 'C';
         }
         console.log(str);
       }
