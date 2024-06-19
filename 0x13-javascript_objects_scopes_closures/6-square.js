c6d02439742d35ed7dfb35392a14a3e005b16cd4
fuadhasen <fuadhas6634@gmail.com>
2024-06-19 10:23:23 -0700

    [update] task 6: sqrt

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ class Square extends Squares {
       for (let i = 0; i < this.height; i++) {
         let str = '';
         for (let j = 0; j < this.width; j++) {
-          str += c
+          str += c;
         }
         console.log(str);
       }
