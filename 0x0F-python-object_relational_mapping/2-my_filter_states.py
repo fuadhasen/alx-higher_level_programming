e2903522e8a4daf0f41a89298bb7741987b04ad7
fuadhasen <fuadhas6634@gmail.com>
2024-06-22 01:29:53 +0530

    [update] task 2: filter_by_userinput

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""python script that filter states from hbtn_0e_0_usa database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_usrs = argv[1]
+    mysql_pwd = argv[2]
+    mydb = argv[3]
+    state = argv[4]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=mysql_usrs,
+                         passwd=mysql_pwd,
+                         db=mydb)
+
+    cursor = db.cursor()
+    query = """
+            SELECT * FROM `states`
+            where name = '{}'
+            ORDER BY states.id
+            """.format(state)
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
