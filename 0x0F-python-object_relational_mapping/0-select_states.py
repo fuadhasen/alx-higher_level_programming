1646e56d1ad95b47815154bf680f7b92e87d04b5
fuadhasen <fuadhas6634@gmail.com>
2024-06-21 14:01:24 +0530

    [create] task 0: select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,12 @@
 import sys
 import MySQLdb
 
-mysql_usr = sys.argv[1]
+mysql_usrs = sys.argv[1]
 mysql_pwd = sys.argv[2]
 db = sys.argv[3]
 
 if __name__ == '__main__':
-    db = MySQLdb.connect('localhost', mysql_usr, mysql_pwd, db)
+    db = MySQLdb.connect('localhost', mysql_usrs, mysql_pwd, db)
     cursor = db.cursor()
     query = "SELECT * FROM `states`"
     cursor.execute(query)
