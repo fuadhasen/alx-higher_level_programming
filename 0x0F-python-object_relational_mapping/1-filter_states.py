2af50dc4aed5c6f3f8ecdc5f362f7d5420245539
fuadhasen <fuadhas6634@gmail.com>
2024-06-21 20:57:57 +0530

    [add] task 1: filter_states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""python script that list all states from hbtn_0e_0_usa db"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_usrs = argv[1]
+    mysql_pwd = argv[2]
+    mydb = argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=mysql_usrs,
+                         passwd=mysql_pwd,
+                         db=mydb)
+
+    cursor = db.cursor()
+    query = """SELECT * FROM `states`
+            where LEFT(`name`, 1) = 'N'
+            ORDER BY states.id
+            """
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
