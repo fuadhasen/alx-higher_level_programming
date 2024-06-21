a6964abb878cdd8044c47f28070512a0533ccd2a
fuadhasen <fuadhas6634@gmail.com>
2024-06-21 13:58:58 +0530

    [create] task 0: select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+mysql_usr = sys.argv[1]
+mysql_pwd = sys.argv[2]
+db = sys.argv[3]
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', mysql_usr, mysql_pwd, db)
+    cursor = db.cursor()
+    query = "SELECT * FROM `states`"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
