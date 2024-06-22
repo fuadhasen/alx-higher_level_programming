9d76e04d36589b88655f0e140ea5f7c466c01fc0
fuadhasen <fuadhas6634@gmail.com>
2024-06-22 11:51:53 +0530

    [add] task 4: cities_bystate

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""python script sql injection free from hbtn_0e_0_usa database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_usrs = argv[1]
+    mysql_pwd = argv[2]
+    mydb = argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=mysql_usrs,
+                         passwd=mysql_pwd,
+                         db=mydb)
+
+    cursor = db.cursor()
+    query = """
+            SELECT cities.id, cities.name, states.name
+            FROM cities
+            LEFT JOIN states
+            ON states.id = cities.state_id
+            ORDER BY cities.id
+            """
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
