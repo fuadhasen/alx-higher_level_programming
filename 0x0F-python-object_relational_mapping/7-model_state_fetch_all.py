bfc3b4631c3d12942243a8484ef3752d22d7c11a
fuadhasen <fuadhas6634@gmail.com>
2024-06-23 13:45:05 +0530

    [update] task 7: model_state_fetch

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
 
     states = session.query(State).order_by(State.id)
     for state in states:
-        print(f'{state.id} : {state.name}')
+        print(f'{state.id}: {state.name}')
     session.close()
