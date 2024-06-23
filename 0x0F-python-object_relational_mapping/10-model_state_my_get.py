7b7da1c26c929a025684d7d8c0cc201aa8590ff7
fuadhasen <fuadhas6634@gmail.com>
2024-06-23 14:18:37 +0530

    [add] task 10: filter by name

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+""" script filter by state name """
 
-import sqlalchemy
 import sys
 from model_state import Base, State
-from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 
 if __name__ == "__main__":
     user = sys.argv[1]
@@ -17,8 +15,8 @@ if __name__ == "__main__":
     else:
         state_name = ''
 
-    engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{mydb}', 
-                           pool_pre_ping=True)
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{mydb}')
     Session = sessionmaker(bind=engine)
     session = Session()
 
