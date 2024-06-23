57eafe08e0629ef55228105c0a152073c6c0d49a
fuadhasen <fuadhas6634@gmail.com>
2024-06-23 14:30:50 +0530

    [add] task 11: add state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+""" script that  Add a new state """
 
-import sqlalchemy
 import sys
 from model_state import Base, State
-from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 
 if __name__ == "__main__":
     user = sys.argv[1]
@@ -14,7 +12,8 @@ if __name__ == "__main__":
     mydb = sys.argv[3]
     
 
-    engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{mydb}')
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{mydb}')
     Session = sessionmaker(bind=engine)
     session = Session()
 
