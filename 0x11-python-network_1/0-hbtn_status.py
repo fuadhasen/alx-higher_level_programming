ef9bc718922d447b15ead408e0b04475f98d3420
fuadhasen <fuadhas6634@gmail.com>
2024-07-08 13:32:08 +0530

    [update] task0: fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@
 
 import urllib.request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-utf8_content = content.decode('utf-8')
-print('Body response:')
-print(f'    - type:', type(content))
-print(f'    - content:', content)
-print(f'    - utf8 content:', utf8_content)
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+    utf8_content = content.decode('utf-8')
+    print('Body response:')
+    print(f'    - type:', type(content))
+    print(f'    - content:', content)
+    print(f'    - utf8 content:', utf8_content)
