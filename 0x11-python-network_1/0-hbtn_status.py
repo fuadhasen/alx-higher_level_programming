ff1ddb69623174447db2dc152222a69f8f4e3811
fuadhasen <fuadhas6634@gmail.com>
2024-07-08 13:22:02 +0530

    [add] task 0: fetch url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" python script that fetch url """
+
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+print('Body response:')
+print(f'    - type:', type(content))
+print(f'    - content:', content)
+print(f'    - utf8 content:', utf8_content)
