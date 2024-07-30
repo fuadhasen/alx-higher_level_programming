3b3262380bdc3e72773f711f7bf20b46e8b93041
fuadhasen <fuadhas6634@gmail.com>
2024-07-30 08:16:11 +0000

    add task3: Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${args[0]}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(`code: ${error.message}`);
+  } else {
+    const parsedBody = JSON.parse(body);
+    console.log(`code: ${parsedBody.title}`);
+  }
+});
