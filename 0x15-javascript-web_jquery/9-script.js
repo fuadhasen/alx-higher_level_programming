13e2763bfccf025eb8a5b0df13d47a539f46e108
fuadhasen <fuadhas6634@gmail.com>
2024-08-08 00:04:42 +0530

    update task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-$.ajax({
-    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-    method: 'GET',
-    success: function (response) {
-        $('DIV#hello').html(response.hello);
-    }    
+// we have to ensure dom is fully loaded first
+$(document).ready(function () {
+
+    $.ajax({
+        url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+        method: 'GET',
+        success: function (response) {
+            $('DIV#hello').html(response.hello);
+        }    
+    });
 });
