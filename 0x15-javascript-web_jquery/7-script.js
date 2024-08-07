23eb5d9bc60460070ba8f3943ea72147fa8faae2
fuadhasen <fuadhas6634@gmail.com>
2024-08-07 21:49:33 +0530

    add task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+$.ajax({
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  method: 'GET',
+  success: function (response) {
+    $('DIV#character').text(JSON.stringify(response.name));
+  }
+});
